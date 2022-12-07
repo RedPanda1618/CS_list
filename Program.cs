f0b3205f807449a2c1e773f152218362fca93a6b
RedPanda1618 <shumpei116@gmail.com>
2022-12-07 20:59:37 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class CSList
+{
+    public static void Main()
+    {
+        // 1次元配列のスライス
+        int[] arr1d = new int[] { 0, 1, 2, 3, 4 };
+        int start = 1, end = 4;
+        var slice1d = arr1d[start..end];
+
+        Console.WriteLine(String.Join(", ", slice1d));
+
+
+        // 2次元配列のスライス
+        int[,] arr2d = new int[4, 4] {
+            { 0, 1, 2, 3 },
+            { 4, 5, 6, 7 },
+            { 8, 9, 10, 11},
+            { 12, 11, 13, 14 } };
+
+        // [行方向]
+        int row = 1;
+        start = 1;
+        end = 3;
+
+        var slice2d_row = new int[end - start];
+        for (int i = 0; i < (end - start); i++)
+        {
+            slice2d_row[i] = arr2d[row, (start - i)];
+        }
+
+        Console.WriteLine(String.Join(", ", slice2d_row));
+
+
+        // [列方向]
+        int col = 2;
+        start = 1;
+        end = 3;
+
+        var slice2d_col = new int[end - start];
+        for (int i = 0; i < (end - start); i++)
+        {
+            slice2d_col[i] = arr2d[(start + i), col];
+        }
+
+        Console.WriteLine(String.Join(", ", slice2d_col));
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# CS_list
+# CSList
+
+## Overview
+
+C#における配列のスライス方法のメモ(自分用)．
+
+１次元の場合は **arr[start : end]** でスライスが可能．
+
+一方，多次元配列の場合は **for 文** を使用して配列に代入していく．
+
+## Reference
+
+- [Microsoft Learn1 次元配列 (C# プログラミング ガイド)] (https://learn.microsoft.com/ja-jp/dotnet/csharp/programming-guide/arrays/single-dimensional-arrays)
